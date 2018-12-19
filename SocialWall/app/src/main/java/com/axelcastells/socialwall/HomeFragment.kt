e3ba3a2a7c566534d152d6327f4380874a909839
Marc Perea i Agustí <marcpa95@gmail.com>
2018-12-19 18:49:01 +0100

    La App rep els missatges enviats

--- SocialWall/app/src/main/java/com/axelcastells/socialwall/HomeFragment.kt
+++ SocialWall/app/src/main/java/com/axelcastells/socialwall/HomeFragment.kt
@@ -3,9 +3,11 @@ package com.axelcastells.socialwall
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass.
@@ -21,5 +23,12 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // TODO: Your code goes here
+        SendMessageButton.setOnClickListener {
+            val userText = TextCapsule.text.toString()
+            Log.i("HomeFragment", "Text Message: "+userText)
+        }
+    }
 }
