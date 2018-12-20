53f4ae4d92e9a8f6a0a10a21981d08fc5de1bb45
Marc Perea i Agustí <marcpa95@gmail.com>
2018-12-20 19:23:36 +0100

    Layouts done + MessageModel updated
    Estan acabats els layouts de sign up i log in. Tots els marges i els strings estan als fitxers "dimens.xml" i "strings.xml" respecticament, res hardcodejat. He actualitzat la class MessageModel.

--- SocialWall/app/src/main/java/com/axelcastells/socialwall/MessageModel.kt
+++ SocialWall/app/src/main/java/com/axelcastells/socialwall/MessageModel.kt
@@ -2,6 +2,13 @@ package com.axelcastells.socialwall
 
 import java.util.*
 
-data class MessageModel(var text: String? = null, var createdAt: Date? = null){
+data class MessageModel(
+    // Message Model
+    var username: String? = null,
+    var text: String? = null,
+    var createdAt: Date? = null,
+    var likes: Int? = null,
+    var userId: String? = null
+) {
 
 }
