56270e44baab56892a3e5d0afdbf43ee4e275942
Marc Perea i Agustí <marcpa95@gmail.com>
2019-01-08 20:00:39 +0100

    StreamPad
    Commit 1 amb codi de classe (amb les crides a la API) + main layout

---
+++ StreamPad/app/src/main/java/com/perea/marc/streampad/network/APIService.kt
@@ -0,0 +1,33 @@
+package com.perea.marc.streampad.network
+
+
+import com.perea.marc.streampad.model.TWStreamResponse
+import com.perea.marc.streampad.model.TWStream
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @Headers("Client-ID: ywvglt0gib8rqdly0ejobehqfi071m")
+    @GET("streams")
+    fun getStreams(): Call<TWStreamResponse>
+
+
+    @Headers("Client-ID: ywvglt0gib8rqdly0ejobehqfi071m")
+    @GET("games")
+    fun getGames(@Query("id") gameId: String): Call<Any>
+
+    companion object {
+        private var retrofit = Retrofit.Builder()
+            .baseUrl("https://api.twitch.tv/helix/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create<ApiService>(ApiService::class.java)
+    }
+
+}
